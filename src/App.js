0cc6ac8b412b565ce5676770e1c077e5b3c0da05
Web-Tech-Codes <arif.kashmiree@gmail.com>
2023-04-17 14:13:16 +0500

    Init

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function MyHeader(){
+  return(
+    <div className='Header'>
+      <div className='TitleLine'><img className='logo' src={logo}/> DTS</div>
+        <ul className='Nav'>
+          <li><a href=''>Home</a></li>
+          <li><a href=''>About</a></li>
+          <li><a href=''>Contact</a></li>
+        </ul>
+    </div>
+  )
+}
+
+function Content(){
+  return(
+    <div className='Content'>
+      <h2>Welcome to my site</h2>
+      <p>Cupidatat magna adipisicing aute officia. Consectetur sit tempor laborum non culpa. Magna tempor aute esse non tempor officia. Occaecat eu ut ut incididunt sunt exercitation tempor magna laboris eu esse exercitation non. Occaecat laboris anim dolore ex. Enim irure aliqua reprehenderit aute esse dolor incididunt deserunt qui laborum veniam minim duis nulla.</p>
+    </div>
+  )
+}
+
+function Sidebar(){
+  return(
+  <div className='Sidebar'>
+    <h2>Recent Events</h2>
+    <p>Labore incididunt consectetur occaecat consequat fugiat. Eiusmod ut do duis ut esse excepteur cupidatat culpa voluptate duis. Excepteur sit proident enim id occaecat laborum voluptate enim anim reprehenderit do esse. Occaecat excepteur nostrud ullamco ex sint nisi dolor ad.</p>
+  </div>
+  )
+}
+
+function Footer(){
+  return(
+    <footer>&copy; 2023 </footer>
+  )
+}
+
+function App() {
+  return (
+    <div className="Container">
+      <MyHeader />
+      <div className='MainContent'>
+        <Content />
+        <Sidebar />
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
